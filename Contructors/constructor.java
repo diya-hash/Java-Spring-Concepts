03506c63a1d2a6d54a054bfc6e5537ed44fd6849
Diya <shaikh.diya@northsouth.edu>
2024-10-23 22:22:21 -0400

    added Constructor example

---
+++ Contructors/constructor.java
@@ -0,0 +1,14 @@
+public class Circle {
+  int r;
+
+  public Main(int y) {
+    radius = r;
+  }
+
+  public static void main(String[] args) {
+    Circle myObj = new Circle(5);
+    System.out.println(myObj.x);
+  }
+}
+
+// Outputs 5
